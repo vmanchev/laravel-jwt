a2e88fd8af7a072abccd6cbc31d29e584c6c1410
Venelin Manchev <vmanchev@santech.fr>
2017-05-14 14:49:30 +0300

    routes description in /

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,134 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>        
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <title>Books API</title>
 
-            .position-ref {
-                position: relative;
-            }
+  </head>
+  <body>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <div class="container">
 
-            .content {
-                text-align: center;
-            }
+      <div class="panel-group" id="api" role="tablist" aria-multiselectable="true">
+        <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="heading-users">
+            <h4 class="panel-title">
+              <a role="button" data-toggle="collapse" data-parent="#api" href="#api-users" aria-expanded="true" aria-controls="api-users">
+                Users
+              </a>
+            </h4>
+          </div>
+          <div id="api-users" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-users">
+            <div class="panel-body">
+              <table class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th class="col-sm-1">Method</th>
+                    <th class="col-sm-5">URI</th>
+                    <td class="col-sm-6">Description</td>
+                  </tr>
+                </thead>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <tbody>
+                  <tr>
+                    <td>POST</td>
+                    <td>/api/auth/signup</td>
+                    <td>Create a new user account</td>
+                  </tr>
+                  <tr>
+                    <td>POST</td>
+                    <td>/api/auth/login</td>
+                    <td>Authenticate user</td>
+                  </tr>
+                  <tr>
+                    <td>POST</td>
+                    <td>/api/auth/recovery</td>
+                    <td>Forgot password</td>
+                  </tr>
+                  <tr>
+                    <td>POST</td>
+                    <td>/api/auth/reset</td>
+                    <td>Set new password</td>
+                  </tr>
+                </tbody>
+              </table>        
             </div>
+          </div>
         </div>
-    </body>
+        <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="heading-books">
+            <h4 class="panel-title">
+              <a role="button" data-toggle="collapse" data-parent="#api" href="#api-books" aria-expanded="true" aria-controls="api-books">
+                Books
+              </a>
+            </h4>
+          </div>
+          <div id="api-books" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-books">
+            <div class="panel-body">
+              <table class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th class="col-sm-1">Method</th>
+                    <th class="col-sm-5">URI</th>
+                    <td class="col-sm-6">Description</td>
+                  </tr>
+                </thead>
+
+                <tbody>
+
+                  <tr>
+                    <td>POST</td>
+                    <td>/api/books</td>
+                    <td>Add a new book</td>
+                  </tr>
+
+                  <tr>
+                    <td>GET</td>
+                    <td>/api/books</td>
+                    <td>Get all books</td>
+                  </tr>
+
+                  <tr>
+                    <td>GET</td>
+                    <td>/api/books/:id</td>
+                    <td>Get a book by id</td>
+                  </tr>
+
+                  <tr>
+                    <td>PUT</td>
+                    <td>/api/books/:id</td>
+                    <td>Update a book</td>
+                  </tr>
+
+                  <tr>
+                    <td>DELETE</td>
+                    <td>/api/books/:id</td>
+                    <td>Delete a book</td>
+                  </tr>
+
+                </tbody>
+              </table>        
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+  </body>
 </html>
