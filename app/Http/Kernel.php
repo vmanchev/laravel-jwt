5f03b80857cd616aabdb0e011325eb8a1da4aef1
Venelin Manchev <vmanchev@santech.fr>
2017-05-14 17:06:51 +0300

    enable cors for all routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
